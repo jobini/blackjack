cf97833cf8d426813892f253d827ce7f01bb6168
Jobin <jobsism19@gmail.com>
2016-09-25 14:04:02 +0530

    Define Player class and define some game functions

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class Player:
 
-    def __init__(self,bet,winnings):
+    def __init__(self,bet,winnings=0,deck=[]):
         self.bet = bet
         self.winnings = winnings
--- gamefunctions.py
+++ gamefunctions.py
@@ -1,5 +1,24 @@
 from classes import Player
 
 def new_game():
-    bet = int(raw_input("Place your bet amount: "))
+    bet = place_bet()
     p = Player(bet,0)
+
+    update()
+
+    action = ask_action()
+
+def place_bet():
+    bet = int(raw_input("Place your bet amount: "))
+    return bet
+
+def update():
+    pass
+
+def ask_action():
+    action_set = set({'h','st','d','sp'})
+    action = raw_input("Your action [Hit:'h', Stand:'st', Double:'d', Split:'sp']: ")
+    while action not in action_set:
+        print "Invalid action! Try again."
+        action = ask_action()
+    return action
